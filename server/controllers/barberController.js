79f1e4293b952318c39991a725f8f55cae7d05aa
Dennis Yehezkiel <dennisyehezkiel.m@gmail.com>
2022-02-16 23:03:59 +0700

    di sini nih yang aku salah ketik di server, mon maap ye

--- server/controllers/barberController.js
+++ server/controllers/barberController.js
@@ -2,36 +2,33 @@ const { Barber, BarberLocation } = require("../models/index");
 const Redis = require("ioredis");
 const redis = new Redis();
 
-
 const getBarbers = async (req, res) => {
-
   try {
-    const barbersCache = await redis.get("barbers")
-    if(barbersCache){
-      res.status(200).json(JSON.parse(barbersCache))
-    }else{
+    const barbersCache = await redis.get("barbers");
+    if (barbersCache) {
+      res.status(200).json(JSON.parse(barbersCache));
+    } else {
       const barbers = await Barber.findAll();
       if (barbers) {
-        await redis.set("barbers",JSON.stringify(barbers))
+        await redis.set("barbers", JSON.stringify(barbers));
         res.status(200).json(barbers);
       }
-
     }
   } catch (err) {
     res.status(500).json(err);
   }
 };
-const getBarberById = async(req,res) => {
-  const { id } = req.params
-  try{
+const getBarberById = async (req, res) => {
+  const { id } = req.params;
+  try {
     const barber = await Barber.findOne({
-      where:{id:id},
-    })
-    res.status(200).json(barber)
-  }catch(err){
-    res.status(500).json(err)
+      where: { id: id },
+    });
+    res.status(200).json(barber);
+  } catch (err) {
+    res.status(500).json(err);
   }
-}
+};
 
 const postBarber = async (req, res) => {
   const { name, email, password, phoneNumber } = req.body;
@@ -42,18 +39,17 @@ const postBarber = async (req, res) => {
       res.status(201).json(barber);
     }
   } catch (err) {
-    err.errors.map(el=>{
-      if(el.message === 'Name is required'){
-        res.status(400).json(el)
-      } else if ( el.message === 'Email is required'){
-        res.status(400).json(el)
-      } else if ( el.message === 'Password is required'){
-        res.status(400).json(el)
-      } else if ( el.message === 'Phone Number is required'){
-        res.status(400).json(el)
-      } 
-    })
-    
+    err.errors.map((el) => {
+      if (el.message === "Name is required") {
+        res.status(400).json(el);
+      } else if (el.message === "Email is required") {
+        res.status(400).json(el);
+      } else if (el.message === "Password is required") {
+        res.status(400).json(el);
+      } else if (el.message === "Phone Number is required") {
+        res.status(400).json(el);
+      }
+    });
   }
 };
 module.exports = { getBarbers, postBarber, getBarberById };
