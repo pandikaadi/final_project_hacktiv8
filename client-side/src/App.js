3820e880ae3c87617d4f364a6a6044dc2a6de77e
Dennis Yehezkiel <dennisyehezkiel.m@gmail.com>
2022-02-18 01:07:12 +0700

    adding service itu hati2 pas merge

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -4,6 +4,8 @@ import BookForm from "./Pages/BookForm";
 import LandingPage from "./Pages/LandingPage";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import CardForm from "./Components/CardForm";
+import Home from "./Pages/Home";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/book" element={<BookForm />} />
+        <Route path="/card" element={<CardForm />} />
       </Routes>
     </>
   );
