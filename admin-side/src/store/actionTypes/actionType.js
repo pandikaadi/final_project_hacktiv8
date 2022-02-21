e809d7bedd89e449373ea810fafd7e6afd4ff1a6
Dennis Yehezkiel <dennisyehezkiel.m@gmail.com>
2022-02-22 03:03:48 +0700

    admin findall

--- admin-side/src/store/actionTypes/actionType.js
+++ admin-side/src/store/actionTypes/actionType.js
@@ -1,2 +1,5 @@
 export const SET_REGISTER_BARBER = "set/registerBarber";
 export const SET_REGISTER_ADMIN = "set/registerAdmin";
+export const FETCH_CHARTDATA = "fetch/chartData";
+export const SET_LOADING = "set/loading";
+export const SET_ERROR = "set/error";
